cd032762874ddc906353c2cfd35e62b721cf2cd9
Quinn Krug <quinn.krug@gmail.com>
2020-07-20 15:25:15 -0600

    front end is getting data from DB

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,10 @@
 import { Template } from 'meteor/templating';
+import { Recipes } from '../api/recipes.js';
 
 import './body.html';
 
 Template.body.helpers({
-  recipes: [
-    { name: 'This is recipe 1' },
-    { name: 'This is recipe 2' },
-    { name: 'This is recipe 3' },
-  ],
+  recipes() {
+      return Recipes.find({});
+    },
 });
